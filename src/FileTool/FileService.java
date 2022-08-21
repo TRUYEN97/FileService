c93d894d51e63ab11118b37e28692a7c43f076c6
SQT <samtruyen60@mail.com>
2022-08-21 16:06:30 +0700

    add unzip function, read byte function

--- src/FileTool/FileService.java
+++ src/FileTool/FileService.java
@@ -7,11 +7,15 @@ package FileTool;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -24,13 +28,8 @@ public class FileService {
         if (name == null || data == null || data.isEmpty()) {
             return false;
         }
-        File file = new File(name);
-        file.mkdirs();
-        if (file.exists()) {
-            file.delete();
-        }
-        try {
-            FileWriter writer = new FileWriter(file);
+        File file = initFile(name);
+        try ( FileWriter writer = new FileWriter(file)) {
             writer.write(data);
             writer.flush();
             return true;
@@ -40,96 +39,142 @@ public class FileService {
         }
     }
 
+    public boolean saveFile(String path, byte[] data) {
+        if (path == null || data == null) {
+            return false;
+        }
+        File file = initFile(path);
+        try ( FileOutputStream writer = new FileOutputStream(file)) {
+            writer.write(data);
+            writer.flush();
+            return true;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    private File initFile(String name) {
+        File file = new File(name);
+        file.mkdirs();
+        if (file.exists()) {
+            file.delete();
+        }
+        return file;
+    }
+
     public String readFile(File file) {
         StringBuilder str = new StringBuilder();
         if (!file.exists()) {
             return str.toString();
         }
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader(file));
+        try ( BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 str.append(line).append("\r\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
         }
         return str.toString();
     }
 
-    public boolean zipFile(String file, String zipPath, String detail) {
+    public boolean zipFile(String fileNameInZip, String zipPath, String detail) {
         File zipFile = new File(zipPath);
         if (zipFile.exists() && !zipFile.delete()) {
             return false;
         }
-        ZipOutputStream out = null;
-        try {
-            out = new ZipOutputStream(new FileOutputStream(zipPath));
+        try ( ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipPath))) {
             try ( BufferedOutputStream bos = new BufferedOutputStream(out)) {
-                out.putNextEntry(new ZipEntry(file));
+                out.putNextEntry(new ZipEntry(fileNameInZip));
                 byte[] buf = detail.getBytes();
                 bos.write(buf);
                 bos.flush();
             }
-            out.close();
             return true;
         } catch (IOException ex) {
             ex.printStackTrace();
             return false;
-        } finally {
-            try {
-                if (out == null) {
-                    out.close();
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
         }
     }
 
-    public boolean zipFile(String zipPath, File fileDetail) {
-        if (!fileDetail.exists()) {
-            return false;
-        }
+    public boolean zipFile(String zipPath, File file) {
+        return zipFile(zipPath, new File[]{file});
+    }
+
+    public boolean zipFile(String zipPath, File[] files) {
         File zipFile = new File(zipPath);
         if (zipFile.exists() && !zipFile.delete()) {
             return false;
         }
-        ZipOutputStream out = null;
-        try {
-            out = new ZipOutputStream(new FileOutputStream(zipPath));
+        try ( ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipPath))) {
             try ( BufferedOutputStream bos = new BufferedOutputStream(out)) {
-                out.putNextEntry(new ZipEntry(fileDetail.getName()));
-                BufferedReader reader = new BufferedReader(new FileReader(fileDetail));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    byte[] buf = line.getBytes();
-                    bos.write(buf);
-                    bos.flush();
+                for (File file : files) {
+                    if (!file.exists()) {
+                        return false;
+                    }
+                    out.putNextEntry(new ZipEntry(file.getName()));
+                    try ( FileInputStream reader = new FileInputStream(file)) {
+                        transferFile(reader, bos);
+                    }
+                    out.closeEntry();
                 }
             }
-            out.close();
             return true;
         } catch (IOException ex) {
             ex.printStackTrace();
             return false;
-        } finally {
-            try {
-                if (out == null) {
-                    out.close();
+        }
+    }
+
+    public boolean unzip(String zipFilePath, String destDirectory) {
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        try ( ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath))) {
+            ZipEntry entry;
+            while ((entry = zipIn.getNextEntry()) != null) {
+                String filePath = destDirectory + File.separator + entry.getName();
+                if (!entry.isDirectory()) {
+                    try ( BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(filePath)))) {
+                        transferFile(zipIn, output);
+                    }
+                } else {
+                    File dir = new File(filePath);
+                    dir.mkdirs();
                 }
-            } catch (IOException ex) {
-                ex.printStackTrace();
+                zipIn.closeEntry();
             }
+            return true;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     *
+     *
+     * @param inputStream
+     * @param outputStream
+     * @throws IOException
+     */
+    public void transferFile(InputStream inputStream, OutputStream outputStream) throws IOException {
+        byte[] bytesIn = new byte[4096];
+        int read;
+        while ((read = inputStream.read(bytesIn)) != -1) {
+            outputStream.write(bytesIn, 0, read);
+            outputStream.flush();
+        }
+    }
+
+    public byte[] getByte(String path) {
+        try ( FileInputStream fileInputStream = new FileInputStream(new File(path))) {
+            return fileInputStream.readAllBytes();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
